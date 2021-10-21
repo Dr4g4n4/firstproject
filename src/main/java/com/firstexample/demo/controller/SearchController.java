e67375ed785995bbc1f0d8728e3d0c8417444370
Bojan Galic <bojan.galic@qcerris.com>
2021-10-21 19:54:01 +0200

    Added docker file and implemented sample for error handling

--- src/main/java/com/firstexample/demo/controller/SearchController.java
+++ src/main/java/com/firstexample/demo/controller/SearchController.java
@@ -13,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Controller
@@ -42,110 +44,131 @@ public class SearchController {
   //  }
     @GetMapping("/byModel")
     @ResponseBody
-    public  List<Map<String,Object>> getByModel(@RequestParam String model) {
-
+    public  List<Map<String,Object>> getByModel(@RequestParam(required = false) String model) {
+        if(model==null){
+            return formResponseError("You need to add Queryparam model");
+        }
         return formResponse(carservice.getCarByModel(model));
     }
     @GetMapping("/byBrand")
     @ResponseBody
-    public List<Map<String,Object>> getByBrand(@RequestParam String brand) {
+    public List<Map<String,Object>> getByBrand(@RequestParam(required = false) String brand) {
+        if(brand==null){
+            return formResponseError("You need to add Queryparam brand");
+        }
         return formResponse(carservice.getCarsByBrand(brand));
     }
 
     @GetMapping("/byGreaterMileage")
     @ResponseBody
-    public Collection<Car> getByMileageGT(@RequestParam Integer mileage) {
-        return carservice.getCarByMileageGT(mileage);
+    public List<Map<String,Object>> getByMileageGT(@RequestParam Integer mileage) {
+        return formResponse(carservice.getCarByMileageGT(mileage));
     }
     @GetMapping("/byLowerMileage")
     @ResponseBody
-    public Collection<Car> getByMileageLT(@RequestParam Integer mileage) {
-        return carservice.getCarByMileageLT(mileage);
+    public List<Map<String,Object>> getByMileageLT(@RequestParam Integer mileage) {
+        return formResponse(carservice.getCarByMileageLT(mileage));
     }
 
     @GetMapping("/byMileageRange")
     @ResponseBody
-    public Collection<Car> getByMileageLT(@RequestParam Integer frommileage,@RequestParam Integer tomileage) {
-        return carservice.getCarByMileageBT(frommileage,tomileage);
+    public List<Map<String,Object>> getByMileageLT(@RequestParam Integer frommileage,@RequestParam Integer tomileage) {
+        return formResponse(carservice.getCarByMileageBT(frommileage,tomileage));
     }
 
     @GetMapping("/byGreaterProductionDate")
     @ResponseBody
-    public Collection<Car> getByProductionDateGT(@RequestParam Date date){
-        return carservice.getCarByProductionDateGT(date);
+    public List<Map<String,Object>> getByProductionDateGT(@RequestParam String date)  {
+        try {
+            Date date_c=new SimpleDateFormat("dd/MM/yyyy").parse(date);
+            return formResponse(carservice.getCarByProductionDateGT(date_c));
+        }
+        catch (ParseException pe){
+            return formResponseError("Invalid date type");
+        }
+
     }
 
     @GetMapping("/byLowerProductionDate")
     @ResponseBody
-    public Collection<Car> getCarProductionDateLT(@RequestParam Date date){
-        return carservice.getCarByProductionDateLT(date);
+    public List<Map<String,Object>> getCarProductionDateLT(@RequestParam Date date){
+        return formResponse(carservice.getCarByProductionDateLT(date));
     }
 
     @GetMapping("/byProductionDateRange")
     @ResponseBody
-    public Collection<Car> getCarByProductionDateBT(@RequestParam Date fromdate,@RequestParam Date todate){
-        return carservice.getCarByProductionDateBT(fromdate, todate);
+    public List<Map<String,Object>> getCarByProductionDateBT(@RequestParam Date fromdate,@RequestParam Date todate){
+        return formResponse(carservice.getCarByProductionDateBT(fromdate, todate));
     }
 
     @GetMapping("/byFuelType")
     @ResponseBody
-    public Collection<Car> getCarByFuel(@RequestParam String fuel){
+    public List<Map<String,Object>> getCarByFuel(@RequestParam String fuel){
 
-        return carservice.getCarByFuel(fuel);
+        return formResponse(carservice.getCarByFuel(fuel));
     }
 
     @GetMapping("/byMotorVolume")
     @ResponseBody
-    public Collection<Car> getCarByMotorVolume(@RequestParam int volume){
+    public List<Map<String,Object>> getCarByMotorVolume(@RequestParam(required = false) String volume){
+        if(volume==null){
+            return formResponseError("You need to add Queryparam volume");
+        }
+        try {
+            int volume_c=Integer.parseInt(volume);
+            return  formResponse(carservice.getCarByMotorVolume(volume_c));
+        }
+        catch (Exception e){
+            return formResponseError("Please use integer values");
+        }
 
-        return  carservice.getCarByMotorVolume(volume);
     }
 
     @GetMapping("/byGreaterMotorPower")
     @ResponseBody
-    public Collection<Car> getCarByMotorPowerGT(@RequestParam int mp){
+    public List<Map<String,Object>> getCarByMotorPowerGT(@RequestParam int mp){
 
-        return carservice.getCarByMotorPowerGT(mp);
+        return formResponse(carservice.getCarByMotorPowerGT(mp));
     }
 
     @GetMapping("/byGreaterHorsePower")
     @ResponseBody
-    public Collection<Car> getCarByHorsePowerGT(@RequestParam int hp){
+    public List<Map<String,Object>> getCarByHorsePowerGT(@RequestParam int hp){
 
-        return carservice.getCarByHorsePowerGT(hp);
+        return formResponse(carservice.getCarByHorsePowerGT(hp));
     }
 
     @GetMapping("/byLowerMotorPower")
     @ResponseBody
-    public Collection<Car> getCarByMotorMotorPowerLT(@RequestParam int mp){
+    public List<Map<String,Object>> getCarByMotorMotorPowerLT(@RequestParam int mp){
 
-        return  carservice.getCarByMotorPowerLT(mp);
+        return formResponse(carservice.getCarByMotorPowerLT(mp));
     }
 
     @GetMapping("/byLowerHorsePower")
     @ResponseBody
-    public Collection<Car> getCarByHorsePowerLT(@RequestParam int hp){
+    public List<Map<String,Object>> getCarByHorsePowerLT(@RequestParam int hp){
 
-        return carservice.getCarByHorsePowerLT(hp);
+        return formResponse(carservice.getCarByHorsePowerLT(hp));
     }
 
     @GetMapping("/byMotorPowerRange")
     @ResponseBody
-    public Collection<Car> getCarByMotorPowerBT(@RequestParam int frommp,@RequestParam int tomp){
-        return carservice.getCarByMotorPowerBT(frommp,tomp);
+    public List<Map<String,Object>> getCarByMotorPowerBT(@RequestParam int frommp,@RequestParam int tomp){
+        return formResponse(carservice.getCarByMotorPowerBT(frommp,tomp));
     }
 
     @GetMapping("/byHosePowerRange")
     @ResponseBody
-    public Collection<Car> getCarByHorsePowerBT(@RequestParam int fromhp,@RequestParam int tohp){
-        return carservice.getCarByHorsePowerBT(fromhp,tohp);
+    public List<Map<String,Object>> getCarByHorsePowerBT(@RequestParam int fromhp,@RequestParam int tohp){
+        return formResponse(carservice.getCarByHorsePowerBT(fromhp,tohp));
     }
 
     @GetMapping("/byColor")
     @ResponseBody
-    public Collection<Car> getCarByColor(@RequestParam String color){
+    public List<Map<String,Object>> getCarByColor(@RequestParam String color){
 
-        return carservice.getCarByColor(color);
+        return formResponse(carservice.getCarByColor(color));
     }
 
 
@@ -181,7 +204,15 @@ public class SearchController {
       //  return new ResponseEntity<Object>(entities, HttpStatus.OK);
     }
 
+    private  List<Map<String,Object>> formResponseError(String error){
+        List<Map<String,Object>> liste= new ArrayList<Map<String,Object>>();
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error",error);
+            liste.add(map);
 
+
+        return liste;
+    }
 
 
 
